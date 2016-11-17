3905618f3ecfa9461520b06d1febd0d78f369ee3
Nicholas Car <nicholas.car@ga.gov.au>
2016-11-17 22:52:10 +1100

    fast XML parser, vocab terms lookup table

--- model.py
+++ model.py
@@ -3,7 +3,136 @@ from rdflib import Graph, URIRef, RDF, XSD, Namespace, Literal, BNode
 
 
 class Sample:
-    def __init__:
+    """
+    Sample class
+    """
+
+    """
+    Associates terms in the database with terms in the IGSN codelist vocabulary:
+    http://pid.geoscience.gov.au/def/voc/igsn-codelists
+
+    One of:
+        http://pid.geoscience.gov.au/def/voc/igsn-codelists/accessType
+        http://pid.geoscience.gov.au/def/voc/igsn-codelists/all-concepts
+        http://pid.geoscience.gov.au/def/voc/igsn-codelists/collectionType
+        http://pid.geoscience.gov.au/def/voc/igsn-codelists/contributorType
+        http://pid.geoscience.gov.au/def/voc/igsn-codelists/featureType
+        http://pid.geoscience.gov.au/def/voc/igsn-codelists/geometryType
+        http://pid.geoscience.gov.au/def/voc/igsn-codelists/identifierType
+        http://pid.geoscience.gov.au/def/voc/igsn-codelists/materialType
+        http://pid.geoscience.gov.au/def/voc/igsn-codelists/methodType
+        http://pid.geoscience.gov.au/def/voc/igsn-codelists/relationType
+        http://pid.geoscience.gov.au/def/voc/igsn-codelists/resourceType
+        http://pid.geoscience.gov.au/def/voc/igsn-codelists/sampleType
+        http://pid.geoscience.gov.au/def/voc/igsn-codelists/sridType
+    """
+    TERM_LOOKUP = {
+        'sample_type': {
+            'automated':            'http://vocabulary.odm2.org/specimentype/automated/',
+            'core':                 'http://vocabulary.odm2.org/specimentype/core/',
+            'coreHalfRound':        'http://vocabulary.odm2.org/specimentype/coreHalfRound/',
+            'corePiece':            'http://vocabulary.odm2.org/specimentype/corePiece/',
+            'coreQuarterRound':     'http://vocabulary.odm2.org/specimentype/coreQuarterRound/',
+            'coreSection':          'http://vocabulary.odm2.org/specimentype/coreSection/',
+            'coreSectionHalf':      'http://vocabulary.odm2.org/specimentype/coreSectionHalf/',
+            'coreSub-Piece':        'http://vocabulary.odm2.org/specimentype/coreSub-Piece/',
+            'coreWholeRound':       'http://vocabulary.odm2.org/specimentype/coreWholeRound/',
+            'cuttings':             'http://vocabulary.odm2.org/specimentype/cuttings/',
+            'dredge':               'http://vocabulary.odm2.org/specimentype/dredge/',
+            'foliageDigestion':     'http://vocabulary.odm2.org/specimentype/foliageDigestion/',
+            'foliageLeaching':      'http://vocabulary.odm2.org/specimentype/foliageLeaching/',
+            'forestFloorDigestion': 'http://vocabulary.odm2.org/specimentype/forestFloorDigestion/',
+            'grab':                 'http://vocabulary.odm2.org/specimentype/grab/',
+            'individualSample':     'http://vocabulary.odm2.org/specimentype/individualSample/',
+            'litterFallDigestion':  'http://vocabulary.odm2.org/specimentype/litterFallDigestion/',
+            'orientedCore':         'http://vocabulary.odm2.org/specimentype/orientedCore/',
+            'other':                'http://vocabulary.odm2.org/specimentype/other/',
+            'petriDishDryDeposition': 'http://vocabulary.odm2.org/specimentype/petriDishDryDeposition/',
+            'precipitationBulk':    'http://vocabulary.odm2.org/specimentype/precipitationBulk/',
+            'rockPowder':           'http://vocabulary.odm2.org/specimentype/rockPowder/',
+            'standardReferenceSpecimen': 'http://vocabulary.odm2.org/specimentype/standardReferenceSpecimen/',
+            'terrestrialSection':   'http://vocabulary.odm2.org/specimentype/terrestrialSection/',
+            'thinSection':          'http://vocabulary.odm2.org/specimentype/thinSection/',
+            'unknown':              'http://vocabulary.odm2.org/specimentype/unknown/'
+        },
+        'method_type': {
+            'Auger':        'http://pid.geoscience.gov.au/def/voc/igsn-codelists/Auger',
+            'Blast':        'http://pid.geoscience.gov.au/def/voc/igsn-codelists/Blast',
+            'Box':          'http://pid.geoscience.gov.au/def/voc/igsn-codelists/Box',
+            'ChainBag':     'http://pid.geoscience.gov.au/def/voc/igsn-codelists/ChainBag',
+            'Corer':        'http://pid.geoscience.gov.au/def/voc/igsn-codelists/Corer',
+            'Dredge':       'http://pid.geoscience.gov.au/def/voc/igsn-codelists/Dredge',
+            'Drill':        'http://pid.geoscience.gov.au/def/voc/igsn-codelists/Drill',
+            'FreeFall':     'http://pid.geoscience.gov.au/def/voc/igsn-codelists/FreeFall',
+            'Grab':         'http://pid.geoscience.gov.au/def/voc/igsn-codelists/Grab',
+            'Gravity':      'http://pid.geoscience.gov.au/def/voc/igsn-codelists/Gravity',
+            'GravityGiant': 'http://pid.geoscience.gov.au/def/voc/igsn-codelists/GravityGiant',
+            'Hammer':       'http://pid.geoscience.gov.au/def/voc/igsn-codelists/Hammer',
+            'Hand':         'http://pid.geoscience.gov.au/def/voc/igsn-codelists/Hand',
+            'Kastenlot':    'http://pid.geoscience.gov.au/def/voc/igsn-codelists/Kastenlot',
+            'Knife':        'http://pid.geoscience.gov.au/def/voc/igsn-codelists/Knife',
+            'MOCNESS':      'http://pid.geoscience.gov.au/def/voc/igsn-codelists/MOCNESS',
+            'Multi':        'http://pid.geoscience.gov.au/def/voc/igsn-codelists/Multi',
+            'Net':          'http://pid.geoscience.gov.au/def/voc/igsn-codelists/Net',
+            'OtherMethod':  'http://pid.geoscience.gov.au/def/voc/igsn-codelists/OtherMethod',
+            'Piston':       'http://pid.geoscience.gov.au/def/voc/igsn-codelists/Piston',
+            'PistonGiant':  'http://pid.geoscience.gov.au/def/voc/igsn-codelists/PistonGiant',
+            'Probe':        'http://pid.geoscience.gov.au/def/voc/igsn-codelists/Probe',
+            'Rock':         'http://pid.geoscience.gov.au/def/voc/igsn-codelists/Rock',
+            'Scallop':      'http://pid.geoscience.gov.au/def/voc/igsn-codelists/Scallop',
+            'Scoop':        'http://pid.geoscience.gov.au/def/voc/igsn-codelists/Scoop',
+            'SideSaddle':   'http://pid.geoscience.gov.au/def/voc/igsn-codelists/SideSaddle',
+            'Trap':         'http://pid.geoscience.gov.au/def/voc/igsn-codelists/Trap',
+            'Trawl':        'http://pid.geoscience.gov.au/def/voc/igsn-codelists/Trawl',
+            'TriggerWeight': 'http://pid.geoscience.gov.au/def/voc/igsn-codelists/TriggerWeight',
+            'UnknownMethod': 'http://pid.geoscience.gov.au/def/voc/igsn-codelists/UnknownMethod',
+            'Vibrating':    'http://pid.geoscience.gov.au/def/voc/igsn-codelists/Vibrating'
+        },
+        'material_type': {
+            'air':          'http://vocabulary.odm2.org/medium/air/',
+            'gas':          'http://vocabulary.odm2.org/medium/gas/',
+            'ice':          'http://vocabulary.odm2.org/medium/ice/',
+            'liquidAqueous': 'http://vocabulary.odm2.org/medium/liquidAqueous/',
+            'liquidOrganic': 'http://vocabulary.odm2.org/medium/liquidOrganic/',
+            'mineral':      'http://vocabulary.odm2.org/medium/mineral/',
+            'organism':     'http://vocabulary.odm2.org/medium/organism/',
+            'other':        'http://vocabulary.odm2.org/medium/other/',
+            'particulate':  'http://vocabulary.odm2.org/medium/particulate/',
+            'rock':         'http://vocabulary.odm2.org/medium/rock/',
+            'sediment':     'http://vocabulary.odm2.org/medium/sediment/',
+            'snow':         'http://vocabulary.odm2.org/medium/snow/',
+            'soil':         'http://vocabulary.odm2.org/medium/soil/',
+            'tissue':       'http://vocabulary.odm2.org/medium/tissue/',
+            'unknown':      'http://vocabulary.odm2.org/medium/unknown/'
+        },
+        'state': {
+            'ACT':  'http://www.geonames.org/2177478/',
+            'NT':   'http://www.geonames.org/2064513/',
+            'NSW':  'http://sws.geonames.org/2155400/',
+            'QLD':  'http://www.geonames.org/2152274/',
+            'SA':   'http://www.geonames.org/2061327/',
+            'TAS':  'http://www.geonames.org/2147291/',
+            'VIC':  'http://www.geonames.org/2145234/',
+            'WA':   'http://www.geonames.org/2058645/'
+        },
+        'country': {
+            'AUS': 'http://www.geonames.org/2077456/australia.html'
+        },
+        'lith': {
+            'one': 'ONE',
+            'two': 'TWO',
+        },
+        'entity_type': {
+            'BOTEHOLE': 'borehole',
+            'two': 'TWO',
+        }
+    }
+
+    '''
+
+    '''
+
+    def __init__(self):
         pass
 
     def populate_from_oracle_api(self):
@@ -11,6 +140,8 @@ class Sample:
         Populates this instance with data from the Oracle Samples table API
         :return: None
         """
+        # call API
+        # request.get
         pass
 
     def populate_from_xml_file(self, xml_file):
@@ -22,30 +153,132 @@ class Sample:
         :param xml_file:
         :return: None
         """
-        root = etree.parse(xml_file)
-        self.igsn = root.xpath('//ROW/IGSN/text()')[0]
-        self.sample_id = root.xpath('//ROW/SAMPLEID/text()')[0]
-        self.x = root.xpath('//ROW/GEOM/SDO_POINT/X/text()')[0]
-        self.y = root.xpath('//ROW/GEOM/SDO_POINT/Y/text()')[0]
-        self.z = root.xpath('//ROW/GEOM/SDO_POINT/Z/text()')[0]
-        self.gtype = root.xpath('//ROW/GEOM/SDO_GTYPE/text()')[0]
-        self.srid = root.xpath('//ROW/GEOM/SDO_SRID/text()')[0]
+        for event, elem in etree.iterparse(xml_file):
+            '''
+            <ROWSET>
+             <ROW>
+              <IGSN>AU2648696</IGSN>
+              <SAMPLEID>1905_50252</SAMPLEID>
+              <SAMPLE_TYPE_NEW/>
+              <SAMPLING_METHOD/>
+              <MATERIAL_CLASS/>
+              <SAMPLE_MIN_LONGITUDE/>
+              <SAMPLE_MAX_LONGITUDE/>
+              <SAMPLE_MIN_LATITUDE/>
+              <SAMPLE_MAX_LATITUDE/>
+              <GEOM>
+               <SDO_GTYPE>3001</SDO_GTYPE>
+               <SDO_SRID>8311</SDO_SRID>
+               <SDO_POINT>
+                <X>143.43508333</X>
+                <Y>-26.94486389</Y>
+                <Z>219.453</Z>
+               </SDO_POINT>
+               <SDO_ELEM_INFO/>
+               <SDO_ORDINATES/>
+              </GEOM>
+              <STATEID>QLD</STATEID>
+              <COUNTRY>AUS</COUNTRY>
+              <TOP_DEPTH>843</TOP_DEPTH>
+              <BASE_DEPTH>868</BASE_DEPTH>
+              <STRATNAME/>
+              <AGE/>
+              <REMARK/>
+              <LITHNAME/>
+              <ACQUIREDATE/>
+              <ENTITY_TYPE>BOREHOLE</ENTITY_TYPE>
+              <ENTITYID>TALGEBERRY 4</ENTITYID>
+              <HOLE_MIN_LONGITUDE>143.43508333</HOLE_MIN_LONGITUDE>
+              <HOLE_MAX_LONGITUDE/>
+              <HOLE_MIN_LATITUDE>-26.94486389</HOLE_MIN_LATITUDE>
+              <HOLE_MAX_LATITUDE/>
+              <LOADEDDATE>10-NOV-16</LOADEDDATE>
+              <SAMPLENO>2648696</SAMPLENO>
+              <ENO>15846</ENO>
+             </ROW>
+            </ROWSET>
+            '''
+            if elem.tag == "IGSN":
+                self.igsn = elem.text
+            elif elem.tag == "SAMPLEID":
+                self.sampleid = elem.text
+            elif elem.tag == "SAMPLE_TYPE_NEW":
+                self.sample_type = Sample.TERM_LOOKUP['sample_type'].get(elem.text),
+            elif elem.tag == "SAMPLING_METHOD":
+                self.method_type = Sample.TERM_LOOKUP['method_type'].get(elem.text),
+            elif elem.tag == "MATERIAL_CLASS":
+                self.material_type = Sample.TERM_LOOKUP['material_type'].get(elem.text),
+            elif elem.tag == "SAMPLE_MIN_LONGITUDE":
+                self.long_min = elem.text
+            elif elem.tag == "SAMPLE_MAX_LONGITUDE":
+                self.long_max = elem.text
+            elif elem.tag == "SAMPLE_MIN_LATITUDE":
+                self.lat_min = elem.text
+            elif elem.tag == "SAMPLE_MAX_LATITUDE":
+                self.lat_max = elem.text
+            elif elem.tag == "SDO_GTYPE":
+                self.gtype = elem.text
+            elif elem.tag == "SDO_SRID":
+                self.srid = elem.text
+            elif elem.tag == "X":
+                self.x = elem.text
+            elif elem.tag == "Y":
+                self.y = elem.text
+            elif elem.tag == "Z":
+                self.z = elem.text
+            elif elem.tag == "SDO_ELEM_INFO":
+                self.elem_info = elem.text
+            elif elem.tag == "SDO_ORDINATES":
+                self.ordinates = elem.text
+            elif elem.tag == "STATEID":
+                self.state = Sample.TERM_LOOKUP['state'].get(elem.text),
+            elif elem.tag == "COUNTRY":
+                self.country = Sample.TERM_LOOKUP['country'].get(elem.text),
+            elif elem.tag == "TOP_DEPTH":
+                self.depth_top = elem.text
+            elif elem.tag == "BASE_DEPTH":
+                self.depth_base = elem.text
+            elif elem.tag == "STRATNAME":
+                self.strath = elem.text
+            elif elem.tag == "AGE":
+                self.age = elem.text
+            elif elem.tag == "REMARK":
+                self.remark = elem.text
+            elif elem.tag == "LITHNAME":
+                self.lith = elem.text
+            elif elem.tag == "ACQUIREDATE":
+                self.date_aquire = elem.text
+            elif elem.tag == "ENTITY_TYPE":
+                self.entity_type = elem.text
+            elif elem.tag == "ENTITYID":
+                self.entity_id = elem.text
+            elif elem.tag == "HOLE_MIN_LONGITUDE":
+                self.hole_long_min = elem.text
+            elif elem.tag == "HOLE_MAX_LONGITUDE":
+                self.hole_long_max = elem.text
+            elif elem.tag == "HOLE_MIN_LATITUDE":
+                self.hole_lat_min = elem.text
+            elif elem.tag == "HOLE_MAX_LATITUDE":
+                self.hole_lat_max = elem.text
+            elif elem.tag == "LOADEDDATE":
+                self.date_load = elem.text
+            elif elem.tag == "SAMPLENO":
+                self.sample_no = elem.text
+            elif elem.tag == "ENO":
+                self.entity_no = elem.text
 
-        self.state = root.xpath('//ROW/STATEID/text()')[0]
-        self.country = root.xpath('//ROW/COUNTRY/text()')[0]
+        return True
 
-        #self.date = root.xpath('//ROW/LOADEDDATE/text()')[0]
-
-        return etree.tostring(root)
-
-    def export_as_rdf(self, model_view, format='turtle'):
+    def export_as_rdf(self, model_view='default', format='turtle'):
         """
         Exports this instance in RDF, according to a given model from the list of supported models,
         in a given rdflib RDF format
 
-        :param model_view:
-        :param format:
-        :return:
+        :param model_view: string of one of the model view names available for Sample objects ['igsn', 'dc', '',
+            'default']
+        :param format: string of one of the rdflib serlialization format ['n3', 'nquads', 'nt', 'pretty-xml', 'trig',
+            'trix', 'turtle', 'xml'], from http://rdflib3.readthedocs.io/en/latest/plugin_serializers.html
+        :return: RDF string
         """
 
         g = Graph()
@@ -153,9 +386,18 @@ class Sample:
 
         return g.serialize(format='nt')
 
+    def export_as_igsn_xml(self):
+        """
+        Exports this Sample instance in XML that validates against the IGSN Descriptive Metadata schema from
+        https://github.com/IGSN/metadata/tree/dev/description
+
+        :return: XML string
+        """
+
+
 
 if __name__ == '__main__':
-    i = IGSN()
-    i.readFromOracleXml('sample_eg1.xml')
-    print i.createRdfGraph()
+    s = Sample()
+    s.populate_from_xml_file('test/sample_eg1.xml')
+    #print s.export_as_rdf()
 
