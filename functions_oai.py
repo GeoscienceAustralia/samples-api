fb4997f3d00cc5d9f664298c2cb56ff1b52f305c
Vanessa <vanessa.newey@ga.gov.au>
2016-12-06 17:23:23 +1100

    added parameter checking for OAI requests"

--- functions_oai.py
+++ functions_oai.py
@@ -1,35 +1,76 @@
+# OAI_ARGS = {
+#     'GetRecord': {
+#         'required': ['identifier', 'metadataPrefix'],
+#         'optional': [],
+#         'exclusive': []
+#     },
+#     'GetMetadata': {
+#         'required': ['identifier', 'metadataPrefix'],
+#         'optional': [],
+#         'exclusive': []
+#     },
+#
+#     'Identify': {
+#         'required': [],
+#         'optional': [],
+#         'exclusive': []
+#     },
+#     'ListIdentifiers': {
+#         'required': ['metadataPrefix'],
+#         'optional': ['from', 'until', 'set'],
+#         'exclusive': ['resumptionToken']
+#     },
+#     'ListMetadataFormats': {
+#         'required': [],
+#         'optional': ['identifier'],
+#         'exclusive': []
+#     },
+#     'ListRecords': {
+#         'required': ['metadataPrefix'],
+#         'optional': ['from', 'until', 'set'],
+#         'exclusive': ['resumptionToken']
+#     },
+#     'ListSets': {
+#         'required': [],
+#         'optional': [],
+#         'exclusive': ['resumptionToken']
+#     },
+# }
+
 OAI_ARGS = {
     'GetRecord': {
-        'required': ['identifier', 'metadataPrefix'],
-        'optional': [],
-        'exclusive': []
-    },
+        'identifier':'required',
+        'metadataPrefix':'required'
+        },
+    'GetMetadata': {
+        'identifier':'required',
+        'metadataPrefix':'required'
+        },
+
     'Identify': {
-        'required': [],
-        'optional': [],
-        'exclusive': []
-    },
+        },
+
     'ListIdentifiers': {
-        'required': ['metadataPrefix'],
-        'optional': ['from', 'until', 'set'],
-        'exclusive': ['resumptionToken']
-    },
+        'from':'optional',
+        'until':'optional',
+        'metadataPrefix':'required',
+        'set':'optional',
+        },
+
     'ListMetadataFormats': {
-        'required': [],
-        'optional': ['identifier'],
-        'exclusive': []
-    },
+        'identifier':'optional'
+        },
+
     'ListRecords': {
-        'required': ['metadataPrefix'],
-        'optional': ['from', 'until', 'set'],
-        'exclusive': ['resumptionToken']
-    },
+        'from_':'optional',
+        'until':'optional',
+        'set':'optional',
+        'metadataPrefix':'required',
+        },
+
     'ListSets': {
-        'required': [],
-        'optional': [],
-        'exclusive': ['resumptionToken']
-    },
-}
+        },
+    }
 
 # https://www.openarchives.org/OAI/openarchivesprotocol.html, 3.6 Error and Exception Conditions
 OAI_ERRORS = {
@@ -39,16 +80,49 @@ OAI_ERRORS = {
 }
 
 
-def valid_oai_args(verb, other_args):
-    args = OAI_ARGS.get(verb)
-    if args is None:
+def valid_oai_args(verb):
+
+    argspec = OAI_ARGS.get(verb)
+    if argspec is None:
         raise ParameterError('The OAI verb is not correct. Must be one of {0}'.format(', '.join(OAI_ARGS.iterkeys())))
 
-    return args
+    pass
 
 
 def validate_oai_parameters(args):
-    # validate using OAI_ARGS
+    verb = args['verb']
+    other_args = args.copy()
+    del other_args['verb']
+
+    argspec = OAI_ARGS.get(verb)
+    if argspec is None:
+        raise ParameterError('The OAI verb is not correct. Must be one of {0}'.format(', '.join(OAI_ARGS.iterkeys())))
+
+    exclusive = None
+    for arg_name, arg_type in list(argspec.items()):
+        if arg_type == 'exclusive':
+            exclusive = arg_name
+
+    # check if we have unknown arguments
+    for key, value in list(other_args.items()):
+        if not key in argspec:
+            msg = "Unknown argument: %s" % key
+            raise ParameterError(msg)
+    # first investigate if we have exclusive argument
+    if exclusive in other_args:
+        if len(other_args) > 1: 
+            msg = ("Exclusive argument %s is used but other "
+                   "arguments found." % exclusive)
+            raise ParameterError(msg)
+        return
+    # if not exclusive, check for required
+    for arg_name, arg_type in list(argspec.items()):
+        if arg_type == 'required':
+            msg = "Argument required but not found: %s" % arg_name
+            if not arg_name in other_args:
+                raise ParameterError(msg)
+
+
     pass
 
 
@@ -57,4 +131,10 @@ class ParameterError(ValueError):
 
 
 if __name__ == '__main__':
-    print valid_oai_args('ListIdentifiers', [])
+    args = {
+        'verb':'GetRecord',
+        'identifier':'AU100',
+        'metadataPrefix':'oai_dc'}
+
+    print 'valid_oai_args(args[\'verb\'])', valid_oai_args(args['verb'])
+    print 'validate_oai_parameters(args)', validate_oai_parameters(args)
