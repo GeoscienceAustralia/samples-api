cbb3d42e86d84e0c898b66b08bb2a797cdd36aba
Nicholas Car <nicholas.car@ga.gov.au>
2017-01-14 00:00:20 +1100

    PEP8 only

--- renderers/error.py
+++ renderers/error.py
@@ -1,5 +1,4 @@
-
-#taken from https://github.com/infrae/pyoai/blob/master/src/oaipmh/datestamp.py
+# taken from https://github.com/infrae/pyoai/blob/master/src/oaipmh/datestamp.py
 class ErrorBase(Exception):
     def oainame(self):
         name = self.__class__.__name__
@@ -9,32 +8,42 @@ class ErrorBase(Exception):
         name = name[0].lower() + name[1:]
         return name
 
+
 class BadArgumentError(ErrorBase):
     pass
+
 
 class BadVerbError(ErrorBase):
     pass
 
+
 class BadResumptionTokenError(ErrorBase):
     pass
+
 
 class CannotDisseminateFormatError(ErrorBase):
     pass
 
+
 class IdDoesNotExistError(ErrorBase):
     pass
+
 
 class NoRecordsMatchError(ErrorBase):
     pass
 
+
 class NoMetadataFormatsError(ErrorBase):
     pass
+
 
 class NoSetHierarchyError(ErrorBase):
     pass
 
+
 class UnknownError(ErrorBase):
     pass
+
 
 # errors not defined by OAI-PMH but which can occur in a client when
 # the server is somehow misbehaving
@@ -44,13 +53,15 @@ class ClientError(Exception):
         """
         raise NotImplementedError
 
+
 class XMLSyntaxError(ClientError):
     """The OAI-PMH XML can not be parsed as it is not well-formed.
     """
     def details(self):
         return ("The data delivered by the server could not be parsed, as it "
                 "is not well-formed XML.")
-    
+
+
 class DatestampError(ClientError):
     """The OAI-PMH datestamps were not proper UTC datestamps as by spec.
     """
@@ -58,5 +69,4 @@ class DatestampError(ClientError):
         self.datestamp = datestamp
 
     def details(self):
-        return ("An illegal datestamp was encountered: %s" % self.datestamp)
-    
+        return "An illegal datestamp was encountered: %s" % self.datestamp
