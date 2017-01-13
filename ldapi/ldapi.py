1a9bd48fd2151d916a070a1dd06e372a53caec95
Nicholas Car <nicholas.car@ga.gov.au>
2017-01-13 21:46:36 +1100

    update LDAPI module to match PROMS

--- ldapi/ldapi.py
+++ ldapi/ldapi.py
@@ -5,10 +5,37 @@ import settings
 class LDAPI:
     """
     A class containing static functions to assist with building a Linked Data API
+
+    This class is issued as a Python file, rather than a Git submodule so check the version number before use!
+
+    Version 1.0
     """
+
+    # maps HTTP MIMETYPES to rdflib's RDF parsing formats
+    MIMETYPES_PARSERS = [
+        ('text/turtle', 'turtle'),
+        ('application/rdf+xml', 'xml'),
+        ('application/rdf+json', 'json-ld'),
+        ('application/json', 'json-ld'),
+        ('text/ntriples', 'nt'),
+        ('text/nt', 'nt'),
+        ('text/n3', 'nt')
+    ]
 
     def __init__(self):
         pass
+
+    @staticmethod
+    def get_rdf_mimetypes_list():
+        return [item[0] for item in LDAPI.MIMETYPES_PARSERS]
+
+    @staticmethod
+    def get_rdf_parser_for_mimetype(mimetype):
+        return [item[1] for item in LDAPI.MIMETYPES_PARSERS if item[0] == mimetype][0]
+
+    @staticmethod
+    def get_mimetype_for_rdf_parser(rdf_parser):
+        return [item[0] for item in LDAPI.MIMETYPES_PARSERS if item[1] == rdf_parser][0]
 
     @staticmethod
     def get_file_extension(mimetype):
@@ -48,9 +75,9 @@ class LDAPI:
     @staticmethod
     def valid_view(view, views_formats):
         """
-        Determines whether a requested model view is valid and, if it is, it returns it
+        Determines whether a requested model model is valid and, if it is, it returns it
 
-        :return: view name (string) or False
+        :return: model name (string) or False
         """
         if view is not None:
             if view in views_formats.iterkeys():
@@ -61,22 +88,22 @@ class LDAPI:
                     .format(', '.join(views_formats.iterkeys()))
                 )
         else:
-            # views_formats will give us the default view
+            # views_formats will give us the default model
             return views_formats['default']
 
     @staticmethod
     def valid_format(format, view, views_formats):
         """
-        Determines whether a requested format for a particular model view is valid and, if it is, it returns it
+        Determines whether a requested format for a particular model model is valid and, if it is, it returns it
 
-        :return: view name (string) or False
+        :return: model name (string) or False
         """
         if format is not None:
             if format.replace(' ', '+') in views_formats[view]:
                 return format.replace(' ', '+')
             else:
                 raise LdapiParameterError(
-                    'The _format parameter is invalid. For this model view, format should be one of {0}.'
+                    'The _format parameter is invalid. For this model model, format should be one of {0}.'
                         .format(', '.join(views_formats[view]))
                 )
         else:
@@ -86,16 +113,16 @@ class LDAPI:
     @staticmethod
     def get_valid_view_and_format(view, format, views_formats):
         """
-        If both the view and the format are valid, return them
-        :param view: the model view parameter
+        If both the model and the format are valid, return them
+        :param view: the model model parameter
         :param format: the MIMETYPE format parameter
-        :param views_formats: the allowed views and their formats in this instance
-        :return: valid view and format
+        :param views_formats: the allowed model and their formats in this instance
+        :return: valid model and format
         """
         view = LDAPI.valid_view(view, views_formats)
         format = LDAPI.valid_format(format, view, views_formats)
         if view and format:
-            # return valid view and format
+            # return valid model and format
             return view, format
 
     @staticmethod
@@ -107,14 +134,51 @@ class LDAPI:
         )
 
     @staticmethod
-    def render_templates_alternates(parent_template, views_formats):
-        return render_template(
-            parent_template,
-            base_uri=settings.BASE_URI,
-            web_subfolder=settings.WEB_SUBFOLDER,
-            view='alternates',
-            placed_html=render_template('view_alternates.html', views_formats=views_formats)
+    def is_a_uri(uri_candidate):
+        """
+        Validates a string as a URI
+
+        :param uri_candidate: string
+        :return: True or False
+        """
+        import re
+        # https://gist.github.com/dperini/729294
+        URL_REGEX = re.compile(
+            u"^"
+            # protocol identifier
+            u"(?:(?:https?|ftp)://)"
+            # user:pass authentication
+            u"(?:\S+(?::\S*)?@)?"
+            u"(?:"
+            # IP address exclusion
+            # private & local networks
+            u"(?!(?:10|127)(?:\.\d{1,3}){3})"
+            u"(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})"
+            u"(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})"
+            # IP address dotted notation octets
+            # excludes loopback network 0.0.0.0
+            # excludes reserved space >= 224.0.0.0
+            # excludes network & broadcast addresses
+            # (first & last IP address of each class)
+            u"(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])"
+            u"(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}"
+            u"(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))"
+            u"|"
+            # host name
+            u"(?:(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)"
+            # domain name
+            u"(?:\.(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)*"
+            # TLD identifier
+            u"(?:\.(?:[a-z\u00a1-\uffff]{2,}))"
+            u")"
+            # port number
+            u"(?::\d{2,5})?"
+            # resource path
+            u"(?:/\S*)?"
+            u"$"
+            , re.UNICODE
         )
+        return re.match(URL_REGEX, uri_candidate)
 
 
 class LdapiParameterError(ValueError):
