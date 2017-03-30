2eeb4974559d074b344bd58927131b8ba9f56747
Nicholas Car <nicholas.car@ga.gov.au>
2017-03-30 22:29:02 +1100

    added 6 verb test functions, removed old unused XML files

--- routes/oai_functions.py
+++ routes/oai_functions.py
@@ -49,7 +49,7 @@ OAI_ARGS = {
 
 # https://www.openarchives.org/OAI/openarchivesprotocol.html, 3.6 Error and Exception Conditions
 OAI_ERRORS = {
-    'badArgument': 'Value of the verb argument is not a legal OAI-PMH verb, the verb argument is missing, or the verb '
+    'badArgument': 'Value of the verb argument is not a legal OAI-PMH verb, the verb argument is missing, or the verb '\
                    'argument is repeated.',
 }
 
--- stop.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-sudo kill `ps aux | grep samples-api | grep -v "grep" | head -3 | awk '{print \$2}'`
